2b4e9a6f737d8d449298803696c9d487910b175d
Shama Chaudhry <chaudharyshama37@gmail.com>
2026-05-08 11:40:22 +0530

    file added

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://blogify-shama.vercel.app/"
     ],
     credentials: true
   })
